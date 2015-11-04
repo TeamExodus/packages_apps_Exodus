00c9a1c709aeda2e655cbf7234a7382a68e0e8f0
Allan Hedelain <tristan202@gmail.com>
2015-11-04 07:53:55 +0000

    Settings: Statusbar clock and date customizations [2/2]
    frameworks change base: http://review.exodus-developers.net/#/c/14180/
    
    Change-Id: I7cf7ccf04abdfa990207587a217377c933bf9dfd

--- src/com/exodus/mods/SystemModsFragment.java
+++ src/com/exodus/mods/SystemModsFragment.java
@@ -1,14 +1,17 @@
 package com.exodus.mods;
 
-import android.os.Bundle;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class SystemModsFragment extends SettingsPreferenceFragment {
+public class SystemModsFragment extends SettingsPreferenceFragment implements 
+    com.android.settings.search.Indexable {
 
     @Override
     protected int getMetricsCategory() {
@@ -25,4 +28,31 @@ public class SystemModsFragment extends SettingsPreferenceFragment {
     public boolean onPreferenceTreeClick(Preference preference) {
         return super.onPreferenceTreeClick(preference);
     }
+
+    /**
+     * For Search.
+     */
+    public static final com.android.settings.search.Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new com.android.settings.search.BaseSearchIndexProvider() {
+            @Override
+            public List<android.provider.SearchIndexableResource> getXmlResourcesToIndex(
+                    Context context, boolean enabled) {
+                android.provider.SearchIndexableResource sir = new android.provider.SearchIndexableResource(context);
+                sir.xmlResId = com.android.settings.R.xml.exodus_android_settings;
+                return Arrays.asList(sir);
+            }
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                final ArrayList<String> result = new ArrayList<String>();
+
+                final android.os.UserManager um = (android.os.UserManager) context.getSystemService(Context.USER_SERVICE);
+                final int myUserId = android.os.UserHandle.myUserId();
+                final boolean isSecondaryUser = myUserId != android.os.UserHandle.USER_OWNER;
+
+                // TODO: Implement search index provider.
+
+                return result;
+            }
+        };
 }
---
+++ src/com/exodus/mods/statusbar/StatusBarMods.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013 Slimroms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exodus.mods.statusbar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class StatusBarMods extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.WIRELESS;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(com.android.settings.R.xml.status_bar_mods);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
+    }
+}

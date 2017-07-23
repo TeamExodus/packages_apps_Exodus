b23221827a54acd71dd363d788cb80e4e9b2548f
Raja M <Raja1612.M@gmail.com>
2017-07-24 01:42:54 +0530

    Revert "gestures: add support for custom gestures"
    This reverts commit 7f7e9ba7e325912e0c044b18a986830be1cc5fc4.
    
    Change-Id: I97391f361128d02fb55caff6c58d0203091bcb21

--- src/com/exodus/mods/SystemModsFragment.java
+++ src/com/exodus/mods/SystemModsFragment.java
@@ -3,7 +3,6 @@ package com.exodus.mods;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -14,11 +13,6 @@ import java.util.List;
 public class SystemModsFragment extends SettingsPreferenceFragment implements 
     com.android.settings.search.Indexable {
 
-    private static final String KEY_ONEPLUS_GESTURES = "oneplus_gestures";
-    private static final String KEY_ONEPLUS_GESTURES_PACKAGE_NAME = "com.cyanogenmod.settings.device";
-
-    private PreferenceScreen mOneplusGestures;
-
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.WIRELESS;
@@ -28,13 +22,6 @@ public class SystemModsFragment extends SettingsPreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(com.android.settings.R.xml.exodus_android_settings);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-
-        mOneplusGestures = (PreferenceScreen) findPreference(KEY_ONEPLUS_GESTURES);
-        if (!com.android.settings.DevelopmentSettings.isPackageInstalled(getActivity(), KEY_ONEPLUS_GESTURES_PACKAGE_NAME)) {
-            prefSet.removePreference(mOneplusGestures);
-        }
     }
 
     @Override
